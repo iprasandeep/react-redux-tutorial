3f10d161750a46b299ac7b58e9d77b046bd442d6
Prasandeep <prasandeepp@gmail.com>
2023-11-18 22:33:55 +0530

    Warning:React17.. issue fixed!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store/store";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import App from './App';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+
+const rootInstance = createRoot(root);
+rootInstance.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
